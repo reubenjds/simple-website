8f06de4b2dbceb0cceb5381e486baffef8bde6a2
reubenjds <reubenjds@gmail.com>
2023-09-19 21:11:43 -0400

    added light and dark mode

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -1,6 +1,17 @@
 function Theme () {
+
+    const handleButton = () => {
+        const body = document.querySelector("body");
+        const button = document.querySelector("button");
+        if (body.getAttribute("data-theme") == "dark") {
+            body.setAttribute("data-theme", "light");
+        } else if (body.getAttribute("data-theme") == "light") {
+            body.setAttribute("data-theme", "dark");
+            button.setAttribute("tetx")
+        } 
+    }
     return ( 
-        <button class='abs'>Button</button>
+        <button class='abs' onClick={handleButton}>Button</button>
      );
 }
 
