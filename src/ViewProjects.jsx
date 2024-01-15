51752d546f78f1abf45ec5bbc29466f849931032
Reuben D'Souza <reubenjds@gmail.com>
2024-01-15 13:40:09 -0500

    fix: change a to link

---
+++ src/ViewProjects.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function ViewProjects() {
+	return (
+		<Link to='projects' class='button projects'>
+			View Projects
+		</Link>
+	);
+}
+
+export default ViewProjects;
