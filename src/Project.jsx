00c6c227ea6e884e9c61d2fd695e49dad8590e12
Reuben D'Souza <reubenjds@gmail.com>
2024-01-16 15:43:45 -0500

    feat: add project template

---
+++ src/Project.jsx
@@ -0,0 +1,18 @@
+function Project(props) {
+	return (
+		<div class='scroll-area'>
+			<div class='flexrow'>
+				<img class='projectimg' src={"../assets/pokedex.png"} />
+				<div class='flexcol'>
+					<div class='projectheading'>Pokedex</div>
+					<div>
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry.
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Project;
