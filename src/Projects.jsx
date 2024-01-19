2f013ec258093f01ddb924f0adda542cc6adc945
Reuben D'Souza <reubenjds@gmail.com>
2024-01-19 16:04:42 -0500

    feat: map Projects json to Project component

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import Theme from "./Theme";
 import Project from "./Project";
+import Projs from "../assets/Projects.json";
 
 export default function Projects() {
 	useEffect(() => {
@@ -15,9 +16,16 @@ export default function Projects() {
 		<>
 			<Theme />
 			<div>
-				<div className='scroll-area'> Scroll to view projects!</div>
-				<Project name='Pokedex' thumbnail='pokedex' />
-				<Project name='Moonqwake' thumbnail='moonqwake' />
+				<div class='scroll-area'> Scroll to view projects! </div>
+				{Projs.map((p) => {
+					return (
+						<Project
+							name={p.name}
+							link={p.link}
+							github={p.github ?? null}
+							description={p.description}></Project>
+					);
+				})}
 			</div>
 		</>
 	);
